ed37d393ec4319c3b3e44344f2d70aa86a2df3b2
dorvanojr <dorvano.freire@techlead.com.br>
2024-02-08 00:13:11 -0300

    correções
    correções

--- src/main/java/br/com/gs3/app/controller/ServicoController.java
+++ src/main/java/br/com/gs3/app/controller/ServicoController.java
@@ -20,7 +20,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/servicos-prestados")
 @RequiredArgsConstructor
-public class ServicoPrestadoController {
+public class ServicoController {
 
     private final ClienteRepository clienteRepository;
     private final ServicoPrestadoRepository repository;
